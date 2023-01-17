336201414b7196a02e71e2abd82a32d9232b1ceb
Mateus Abdallah <abdallahmateus@gmail.com>
2023-01-16 23:43:45 -0800

    creating components with routes and links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 
 function App() {
-  return (
-    <Route>
-      
-      <ul>
-        <li>Home</li>
-        <li>Contact</li>
-      </ul>
+  return (    
+    <Router>      
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
       <Switch>
-
-      </Switch>
-      <p>Footer</p>
-    </Route>
+        <Container customClass="min-height">
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/company'>
+            <Company />
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+          <Route exact path='/newproject'>
+            <NewProject />
+          </Route>
+        </Container>
+        
+     </Switch>
+      <p>Footer</p> 
+    </Router>
   );
 }
 
---
+++ src/components/pages/Company.js
@@ -0,0 +1,5 @@
+function Company(){
+    return <h4>Company</h4>
+}
+
+export default Company
